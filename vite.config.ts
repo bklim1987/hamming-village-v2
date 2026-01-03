a3356f2bf61cd6ef8c73b4734d339e939a4c1ad8
bklim1987 <bklim1987@gmail.com>
2026-01-04 03:34:05 +0800

    Added index.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/hamming-village-v2/',
+
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5000,
-  },
-  resolve: {
-    alias: {
-      '@assets': path.resolve(__dirname, 'attached_assets'),
-    },
-  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
